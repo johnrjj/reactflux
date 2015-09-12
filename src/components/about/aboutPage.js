e8ca5c8419d443bd15bf58564f0bdef950215376
John Johnson <johnrjj@gmail.com>
2015-09-12 18:54:52 -0400

    RFLUX-002
    First components
    Simple routing
    Layout

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var About = React.createClass({
+  render: function() {
+    return (
+      <div >
+
+        <h1> About</h1>
+        <p>This is johns program that uses React, React Router, and Flux. This is the about page.</p>
+      </div>
+    );
+  }
+});
+
+
+module.exports = About;
---
+++ src/components/homePage.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+
+        <h1> REACT HOMIES</h1>
+        <p>React, React Router, and Flux</p>
+      </div>
+    );
+  }
+});
+
+
+module.exports = Home;
